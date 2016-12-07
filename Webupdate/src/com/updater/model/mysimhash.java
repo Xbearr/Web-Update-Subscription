9a7e7c0e4bfdbcffb9c40cee7806a1107b9614b2
Xbearr <631319951@qq.com>
2016-12-07 21:43:44 +0800

    修改了simhash算法，让它的准确度更高

--- Webupdate/src/com/updater/model/mysimhash.java
+++ Webupdate/src/com/updater/model/mysimhash.java
@@ -1,15 +1,13 @@
 package com.updater.model;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*; 
+import java.util.regex.*;
 
 public class mysimhash {
 	
-	/*
-	 *将文本中的通用项给去掉
-	 */
-	private String sp="=|\\.|\n|\r| |<html|/html|<h1|</h1>|<h2|/h2|<h3|/h3|<ul|/ul|<br|/br|<head|/head|<li|/li|<div|/div|<a|/a|<p|/p|<body|/body|"
-			+ "<|>|<script|/scirpt|<title|/title|<pre|</pre>|<meta|</meta>|<header|</header>|<span|/span";
+	//通过正则将一些常用的在文本查重中无用的标签去掉
+	private static String sp="=|\\.|。|，|\"|'|\\[|\\]|！|\\!|\\?|/|\n|\r| |<html|/html|<h1|</h1>|<h2|/h2|<h3|/h3|<ul|/ul|<br|/br|<head|/head|<li|/li|<div|/div|<a|/a|<p|/p|<body|/body|"
+			+ "-_-|\\)|\\(|,|\\{|\\}|;|:|--|<|>|<script|/scirpt|<title|/title|<pre|</pre>|<meta|</meta>|<header|</header>|<span|/span|_|#|&|%|-";
 	
 	private String tokens;  //文本
 	
@@ -19,50 +17,103 @@ public class mysimhash {
     
     private int hashbits = 64;
     
-    public mysimhash(String tokens){
-    	this.tokens = tokens;
-        //看错了，这个和类SimHash比较相似，看花眼了
-        this.intSimHash = this.simhash();
-    }
+    private int max=100000;
     
     public mysimhash(String tokens,int hashbits){
-        this.tokens = tokens;
+    	this.tokens=tokens;
         this.hashbits = hashbits;
         this.intSimHash = this.simhash();
     }
-    
-    public BigInteger simhash(){
-    	int[] v = new int[this.hashbits];
+    //用map来保存键值和权重。由于map键值唯一的特性，方便管理
+	public Map<String, Integer> countString(){
+		
+		//先切割字符串，去掉一些分隔符
     	String[] st=this.tokens.split(sp);
-    	String[] s=new String[st.length];
+    	String[] s=new String[max];
 		int k=0;
-		for(int i=0;i<s.length;i++){
+		
+		//去掉因为正则切割而造成的空格，并且将含中文的字符串分开
+		for(int i=0;i<st.length;i++){
 			if (st[i].length()<=0){
 			}
 			else
 			{
-				s[k]=st[i];
-				k++;
+				if(isChinese(st[i])==1){
+					for(int j=0;j<st[i].length();j++){
+						//System.out.println(String.valueOf(st[i].charAt(j)));
+						s[k]=String.valueOf(st[i].charAt(j));
+						k++;
+					}
+				}
+				else{
+					s[k]=st[i];
+					k++;
+				}
 			}
 		}
-    	int i=0;
-    	while(i<k){
-    		String temp=s[i];
-    		BigInteger t = this.hash(temp);
+		
+		Map<String, Integer> hash_Data = new HashMap<String, Integer>();
+		for(int i=0;i<k;i++){
+			String word=s[i];
+			if(hash_Data.containsKey(word)){
+				int t=hash_Data.get(word).intValue()+1;
+				hash_Data.put(word, new Integer(t));
+			}
+			else{
+				hash_Data.put(word, new Integer(1));
+			 }
+		 }
+		 return hash_Data;
+    }
+    
+	//判断是否为中文
+	public int isChinese(String str) {
+        String regEx1 = "[\\u4e00-\\u9fa5]+";
+        String regEx2 = "[\\uFF00-\\uFFEF]+";
+        String regEx3 = "[\\u2E80-\\u2EFF]+";
+        String regEx4 = "[\\u3000-\\u303F]+";
+        String regEx5 = "[\\u31C0-\\u31EF]+";
+        Pattern p1 = Pattern.compile(regEx1);
+        Pattern p2 = Pattern.compile(regEx2);
+        Pattern p3 = Pattern.compile(regEx3);
+        Pattern p4 = Pattern.compile(regEx4);
+        Pattern p5 = Pattern.compile(regEx5);
+        Matcher m1 = p1.matcher(str);
+        Matcher m2 = p2.matcher(str);
+        Matcher m3 = p3.matcher(str);
+        Matcher m4 = p4.matcher(str);
+        Matcher m5 = p5.matcher(str);
+        if (m1.find() || m2.find() || m3.find() || m4.find() || m5.find())
+            return 1;
+        else
+            return 0;
+    }
+	
+	//主函数
+    public BigInteger simhash(){
+    	int[] v = new int[this.hashbits];
+    	Map<String, Integer> hash_Data=countString();
+    	Iterator<String> iter = hash_Data.keySet().iterator();
+    	while(iter.hasNext()){
+    		String word=(String) iter.next();
+    		
+    		//计算hash值，并且按照64位的每一位进行计算，结果保存在v内
+    		BigInteger t = this.hash(word);
     		for(int j=0;j<this.hashbits;j++){
     			BigInteger bitmask = new BigInteger("1").shiftLeft(j);
     			if (t.and(bitmask).signum() != 0) {
-                    v[j] += 1;
+                    v[j] += hash_Data.get(word);
                 } 
     			else {
-                    v[j] -= 1;
+                    v[j] -= hash_Data.get(word);
                 }
     		}
-    		i++;
     	}
+    	
+    	//生成fingerprint
     	BigInteger fingerprint = new BigInteger("0");
     	StringBuffer simHashBuffer = new StringBuffer();
-    	for (i = 0; i < this.hashbits; i++) {
+    	for (int i = 0; i < this.hashbits; i++) {
             if (v[i] >= 0) {
                 fingerprint = fingerprint.add(new BigInteger("1").shiftLeft(i));
                 simHashBuffer.append("1");
@@ -71,11 +122,10 @@ public class mysimhash {
             }
         }
     	this.strSimHash = simHashBuffer.toString();
-        //System.out.println(this.strSimHash + " length " + this.strSimHash.length());
         return fingerprint;
     }
     
-    //计算每一位的哈希码
+    //计算每个字符的hash值
     private BigInteger hash(String source) {
     	if (source == null || source.length() == 0) {
             return new BigInteger("0");
@@ -97,33 +147,7 @@ public class mysimhash {
         }
     }
     
-    public List<BigInteger> subByDistance(mysimhash simHash, int distance){
-    	int numEach = this.hashbits/(distance+1);
-    	List<BigInteger> characters = new ArrayList();
-        
-    	StringBuffer buffer = new StringBuffer();
-
-    	for( int i = 0; i < this.intSimHash.bitLength(); i++){
-        	boolean sr = simHash.intSimHash.testBit(i);
-        	
-        	if(sr){
-        		buffer.append("1");
-        	}	
-        	else{
-        		buffer.append("0");
-        	}
-        	
-        	if( (i+1)%numEach == 0 ){
-            	BigInteger eachValue = new BigInteger(buffer.toString(),2);
-            	System.out.println("----" +eachValue );
-            	buffer.delete(0, buffer.length());
-            	characters.add(eachValue);
-        	}
-        }
-
-    	return characters;
-    }
-    
+    //求两字符串之间的距离
     public int getDistance(String str1, String str2) {  
         int distance;  
         if (str1.length() != str2.length()) {  
@@ -138,47 +162,4 @@ public class mysimhash {
         }  
         return distance;  
     }
-    
-    public int hammingDistance(mysimhash other) {
-    	
-        BigInteger x = this.intSimHash.xor(other.intSimHash);
-        int tot = 0;
-        
-        //统计x中二进制位数为1的个数
-        //我们想想，一个二进制数减去1，那么，从最后那个1（包括那个1）后面的数字全都反了，对吧，然后，n&(n-1)就相当于把后面的数字清0，
-        //我们看n能做多少次这样的操作就OK了。
-        
-         while (x.signum() != 0) {
-            tot += 1;
-            x = x.and(x.subtract(new BigInteger("1")));
-        }
-        return tot;
-    }
-    /*
-    public static void main(String[] args) {
-    	 String s = "我爱读书";
-    	 mysimhash hash1=new mysimhash(s,64);
-    	 System.out.println(hash1.intSimHash + "  " + hash1.intSimHash.bitLength());
-    	 hash1.subByDistance(hash1, 3);
-    	 System.out.println("\n");
-         s = "我爱读书，哈哈123";
-         mysimhash hash2 = new mysimhash(s, 64);
-      
-         System.out.println(hash2.intSimHash+ "  " + hash2.intSimHash.bitCount());
-         hash1.subByDistance(hash2, 3);
-         s = "i love bashing";
-         mysimhash hash3 = new mysimhash(s, 64);
-         System.out.println(hash3.intSimHash+ "  " + hash3.intSimHash.bitCount());
-         hash1.subByDistance(hash3, 3);
-         System.out.println("============================");
-         int dis = hash1.getDistance(hash1.strSimHash,hash2.strSimHash);
-         double score=1.0*dis/64;
-         System.out.println(score);
-         System.out.println(hash1.hammingDistance(hash2) + " "+ dis);
-         
-         int dis2 = hash1.getDistance(hash1.strSimHash,hash3.strSimHash);
-         
-         System.out.println(hash1.hammingDistance(hash3) + " " + dis2);
-    }
-    */
 }
